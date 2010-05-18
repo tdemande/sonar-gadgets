cf00666f0f30cfc60dd9b69cc678be84842a8b89
Mark Rekveld <markrekveld@marvelution.com>
2010-05-18 21:27:19 +0000

    [MARVSONARGADGETS-21] Refactored the method to get the Sonar Gadget Ids
    git-svn-id: http://svn.marvelution.com/atlassian/sonar-gadgets/trunk@1555 079872ed-eb71-41de-a42c-5a92345b582c

---
+++ src/main/java/com/marvelution/gadgets/sonar/utils/SonarGadgetsUtils.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to Marvelution under one or more contributor license 
+ * agreements.  See the NOTICE file distributed with this work 
+ * for additional information regarding copyright ownership.
+ * Marvelution licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.marvelution.gadgets.sonar.utils;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.atlassian.sal.api.message.I18nResolver;
+
+/**
+ * Utility class for the Sonar Gadgets
+ * 
+ * @author <a href="mailto:markrekveld@marvelution.com">Mark rekveld</a>
+ * 
+ * @since 1.2.0
+ */
+public class SonarGadgetsUtils {
+
+	private final I18nResolver i18nResolver;
+
+	private static final Pattern GADGET_ID_PATTERN = Pattern.compile("^sonar\\.gadget\\.([a-z]*)\\.title$");
+
+	/**
+	 * Constructor
+	 * 
+	 * @param i18nResolver the {@link I18nResolver} implementation
+	 */
+	public SonarGadgetsUtils(I18nResolver i18nResolver) {
+		this.i18nResolver = i18nResolver;
+	}
+
+	/**
+	 * Get all the Sonar gadget Ids
+	 * 
+	 * @return {@link Collection} with all the Sonar Gadget Ids
+	 */
+	public Collection<String> getGadgetIds() {
+		final Set<String> gadgetIds = new HashSet<String>();
+		final Map<String, String> bundle = i18nResolver.getAllTranslationsForPrefix("sonar.gadget", Locale.ENGLISH);
+		for (final Entry<String, String> entry : bundle.entrySet()) {
+			final Matcher matcher = GADGET_ID_PATTERN.matcher(entry.getKey());
+			if (matcher.find()) {
+				gadgetIds.add(matcher.group(1));
+			}
+		}
+		return gadgetIds;
+	}
+
+}
